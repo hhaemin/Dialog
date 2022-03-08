3665b97fbf22a540dc2718e0ed9228561b0cc94e
haemin <ahrgoals@naver.com>
2022-03-08 18:53:16 +0900

    flag game

---
+++ app/src/main/java/com/example/dialog/CountryItem.java
@@ -0,0 +1,20 @@
+package com.example.dialog;
+
+public class CountryItem {
+
+    String name;
+    int image;
+
+    public CountryItem(String name, int image){
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
---
+++ app/src/main/java/com/example/dialog/Database.java
@@ -0,0 +1,37 @@
+package com.example.dialog;
+
+public class Database {
+
+    Integer[] flags = {
+            R.drawable.flag_netherlands,
+            R.drawable.flag_australia,
+            R.drawable.flag_belgium,
+            R.drawable.flag_bulgaria,
+            R.drawable.flag_canada,
+            R.drawable.flag_denmark,
+            R.drawable.flag_south_korea,
+            R.drawable.flag_france,
+            R.drawable.flag_germany,
+            R.drawable.flag_iceland,
+            R.drawable.flag_india,
+            R.drawable.flag_indonesia,
+            R.drawable.flag_italy,
+    };
+
+    String[] answers = {
+            "netherlands",
+            "australia",
+            "belgium",
+            "bulgaria",
+            "canada",
+            "denmark",
+            "south_korea",
+            "france",
+            "germany",
+            "iceland",
+            "india",
+            "indonesia",
+            "italy"
+
+    };
+}
